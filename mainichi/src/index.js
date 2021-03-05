c415bce22c637ab94b22757047eb9bfbf7eb2c8f
Drew Thompson <drewallynthompson@gmail.com>
2021-03-04 17:07:14 -0700

    creating basic login frame

--- mainichi/src/index.js
+++ mainichi/src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import wordsReducer from './reducers/wordsReducer.js'
 
+const store = createStore(wordsReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
