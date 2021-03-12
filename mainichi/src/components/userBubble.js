edd6c7b9f97c2f0b9c31cd73994a5d50647dd1e3
Drew Thompson <drewallynthompson@gmail.com>
2021-03-11 23:02:51 -0700

    User Components

---
+++ mainichi/src/components/userBubble.js
@@ -0,0 +1,14 @@
+import React, { Component} from 'react';
+import UserRoutes from './components/userRoutes.js'
+
+export default class UserBubble extends Component {
+
+    render(){
+
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ mainichi/src/containers/userContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import UserBubble from './components/userBubble.js';
+
+export default class UserContainer extends Component {
+    render(){
+        return(
+            <div>
+                < UserBubble />
+            </div>
+        )
+    }
+}
