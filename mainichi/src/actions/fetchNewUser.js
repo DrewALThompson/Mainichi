cf561fbffa3c4ba19d41b53cd980711488c36410
Drew Thompson <drewallynthompson@gmail.com>
2021-03-26 04:01:18 -0600

    added another action

--- mainichi/src/actions/fetchNewUser.js
+++ mainichi/src/actions/fetchNewUser.js
@@ -1,6 +1,6 @@
 export function fetchUser(json){
     return (dispatch) => {
-        fetch('http://localhost:3000/sessions')
+        fetch('http://localhost:3000/users')
         .then(res => res.json())
         .then(user => dispatch({
             type: 'FETCH_USER',
